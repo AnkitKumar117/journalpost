79e539d69fbd30e751c5a5b731cc478d7cc85e4b
AnkitKumar117 <ankipandel1234@gmail.com>
2021-01-16 23:50:02 +0530

    Repo_staged

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,13 +2,14 @@
 
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
-export const fetchPosts = () => {
-    return function () {
-        const promise = jsonPlaceholder.get('/posts');
-        return {
-            type: 'FETCH_POST',
-            payload: promise
-        };
-    }
-};
+// export const fetchPosts = () => {
+//     return async function (dispatch , getState) {
+//         const response = await jsonPlaceholder.get('/posts');
+//         dispatch( {type: 'FETCH_POSTS' , payload: response})
+//     };
+// };
 
+export const fetchPosts = () =>  async (dispatch) => {
+        const response = await jsonPlaceholder.get('/posts');
+        dispatch( {type: 'FETCH_POSTS' , payload: response})
+    };
