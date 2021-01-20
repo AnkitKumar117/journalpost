22d543d8ce2b9d6dd87182a3a8cc5243f44c5888
AnkitKumar117 <ankipandel1234@gmail.com>
2021-01-20 16:04:02 +0530

    React

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,5 +1,5 @@
 //actions
-
+import _ from 'lodash';
 import jsonPlaceholder from "../apis/jsonPlaceholder";
 
 // export const fetchPosts = () => {
@@ -9,7 +9,24 @@ import jsonPlaceholder from "../apis/jsonPlaceholder";
 //     };
 // };
 
-export const fetchPosts = () =>  async (dispatch) => {
-        const response = await jsonPlaceholder.get('/posts');
-        dispatch( {type: 'FETCH_POSTS' , payload: response})
-    };
+export const fetchPosts = () => async (dispatch) => {
+    const response = await jsonPlaceholder.get('/posts');
+    dispatch({ type: 'FETCH_POSTS', payload: response.data })
+};
+
+ export const fetchUser = (id) => (dispatch) => {
+     _fetchUser(id,dispatch);
+ };
+
+ const _fetchUser=_.memoize(async (id,dispatch)=>{
+    const response =await jsonPlaceholder.get(`/users/${id}`);
+    dispatch( {type: 'FETCH_USER' , payload: response.data})
+ });
+
+ 
+// export const fetchUser = function(id){
+//     return _.memoize(async function(dispatch) {
+//         const response = await jsonPlaceholder.get(`/users/${id}`);
+//         dispatch({ type: 'FETCH_USER', payload: response.data })
+//     });
+// };
